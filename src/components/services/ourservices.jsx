8ab116226ac10e822e01a803e43e174ccf4cd19b
Ronik <102733678+Ronikrr@users.noreply.github.com>
2024-11-23 13:30:10 +0530

    first commit

---
+++ src/components/services/ourservices.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Ourservices = () => {
+  return (
+      <div className='pt-[105px] data_cla' >
+          <div className="container mx-auto lg:max-w-[71.25rem] pt-[1.5rem] pb-[1rem]">
+              <div className="w-full lg:w-[1140px] mx-auto flex flex-wrap items-center">
+                  <div className="flex items-center pt-[1rem] w-full">
+                      <div className="w-6/12 pt-[1rem]">
+                          <h1 className='text-[30px] lg:text-[60px] capitalize leading-[1.2] font-extrabold' > our <br /> services </h1>
+                      </div>
+                      <div className="w-6/12 pt-[1rem]">
+                          <p className='text-[18px]' >We help businesses reach their full potential by creating a brand identity that is authentic and reflective of them. We understand that every brand comes with its own unique challenges, and hence requires expertise. We join hands with you as your growth partners and focus on delivering a power-packed panga each time!</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default Ourservices
